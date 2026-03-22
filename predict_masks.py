f69e326c5388366f282fe10d2c622ca8ed19994e
Debangi D. <tenderverse.writes@gmail.com>
2026-03-23 04:11:07 +0530

    Refactor mask prediction and save probabilities
    Updated model loading and added probability saving functionality.

--- predict_masks.py
+++ predict_masks.py
@@ -1,32 +1,53 @@
 import torch
-from train_unet import UNet
+from models.unet import UNet   
 from utils.dataset import CamusDataset
 from torch.utils.data import DataLoader
 import cv2
 import os
-
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# dataset
+# ================= DATA =================
 dataset = CamusDataset("data/test/images", "data/test/masks")
 loader = DataLoader(dataset, batch_size=1, shuffle=False)
 
-# model
+# ================= MODEL =================
 model = UNet().to(device)
-model.load_state_dict(torch.load("unet_model.pth", map_location=device))
+model.load_state_dict(torch.load("best_unet_model.pth", map_location=device))
 model.eval()
 
-#  output folder
+# ================= OUTPUT FOLDERS =================
 os.makedirs("predicted_masks", exist_ok=True)
+os.makedirs("predicted_probs", exist_ok=True)  
 
-# predict and save masks
+print("Starting inference...")
+
 with torch.no_grad():
     for i, (img, _) in enumerate(loader):
-        img = img.to(device)
-        pred = model(img)
-        pred = pred.squeeze().cpu().numpy()
-        # save mask
-        cv2.imwrite(f"predicted_masks/mask_{i:04d}.png", (pred * 255).astype("uint8"))
 
-print("All predicted masks saved in 'predicted_masks/'")
+        img = img.to(device)
+
+        # -------- Forward --------
+        pred = model(img)
+        prob = torch.sigmoid(pred)   
+
+        prob_np = prob.squeeze().cpu().numpy()
+
+        # -------- Binary mask --------
+        mask = (prob_np > 0.5).astype(np.float32)
+
+        # -------- Save --------
+        cv2.imwrite(
+            f"predicted_masks/mask_{i:04d}.png",
+            (mask * 255).astype("uint8")
+        )
+
+        cv2.imwrite(
+            f"predicted_probs/prob_{i:04d}.png",
+            (prob_np * 255).astype("uint8")
+        )
+
+        print(f"Saved sample {i}")
+
+print("\nAll predictions saved!")
